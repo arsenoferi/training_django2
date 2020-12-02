0d46f60355d4fb171b14e9066031fd8da9e61995
Arseno Feri Alzahabi <arsenoferi@hotmail.com>
2020-12-02 18:33:36 +0700

    commit 1

---
+++ countword/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+import operator
+def Home(request):
+    return render(request,'home.html',{'seno' : 'My Name Is Seno'})
+
+def About(request):
+    variabel={
+        'Nama':'Arseno Feri Alzahabi',
+        'Jobs': 'Compliance Auditor'
+    }
+    return render(request,'about.html', variabel)
+
+def Count(request):
+    allinput = request.GET['allinput']
+    
+    wordlist = allinput.split()
+    
+    worddict = {}
+    
+    for word in wordlist:
+        if word in worddict:
+            worddict[word]+=1
+        else:
+            worddict[word] = 1
+            
+    count_worddict = sorted(worddict.items(), key=operator.itemgetter(1), reverse=True)
+    
+    nilai = {
+       'fulltext':allinput,
+       'count' : len(wordlist),
+       'worddict' : count_worddict
+    }
+    
+    return render(request,'count.html', nilai)
